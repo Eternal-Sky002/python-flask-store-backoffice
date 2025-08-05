70ffb534a23019dab418954dcab8c0dcaca16c33
ETERNALSKY-002 <wahyu.mahendra@pajak.go.id>
2025-08-05 11:41:08 +0700

    deploy render

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ from modules.item import item_bp
 from modules.store import store_bp
 from modules.tag import tag_bp
 
-app = Flask(__name__)
-app.secret_key = 'your-secret-key-here'
+def create_app():
+        
+    app = Flask(__name__)
+    app.secret_key = 'your-secret-key-here'
 
-# Register blueprints
-app.register_blueprint(user_bp)
-app.register_blueprint(item_bp)
-app.register_blueprint(store_bp)
-app.register_blueprint(tag_bp)
+    # Register blueprints
+    app.register_blueprint(user_bp)
+    app.register_blueprint(item_bp)
+    app.register_blueprint(store_bp)
+    app.register_blueprint(tag_bp)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8001)
+    return app
